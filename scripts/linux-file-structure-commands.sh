a85933a5aa98796deff7aeef9f87eeb3610a8e9c
Asiimwe-Isaac <asiimweisaac40@gmail.com>
2026-04-22 01:41:05 -0700

    I added a script with linux file structure commands

---
+++ scripts/linux-file-structure-commands.sh
@@ -0,0 +1,18 @@
+cd /
+ls -l # give us the full structure of the linux 
+man uptime #tells you how long a system has been running
+uptime
+man free #gives you information on the memory usage
+free
+man ps 
+ps -A #snapchat of all the processes
+man df # file system space usage
+df -h #sizes of space in readable formats rather than using bytes to know the amount of space used
+man fdisk #maniputions of partitons
+fdisk -l #to run information about your partitions
+sudo fdisk -l # gives you permission to access the partiton information
+man lsblk # this lists block devices(lists storage devices along with their partitions sizes and mount points)
+lsblk
+man top #displays linux processes
+sudo apt install htop # downloading htop which is an interactive real time system monitor and process viewer for linux that acts as an enhanced version of the traditional top
+htop
